aa05f287b311161747a52d9c054757073ad729f3
r0man <roman@burningswell.com>
2022-02-10 20:28:08 +0100

    No need to require parseedn

--- src/paimon.el
+++ src/paimon.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/r0man/paimon.el
 ;; Keywords: paimon, search, tools
 ;; Maintainer: r0man <roman@burningswell.com>
-;; Package-Requires: ((aio "1.0") (closql "1.2.0") (emacs "27.1") (emacsql "3.0.0") (emacsql-sqlite "3.0.0") (f "0.20.0") (ht "2.4") (parseedn "1.0.6") (transient "0.3.7") (request "0.3.3"))
+;; Package-Requires: ((aio "1.0") (closql "1.2.0") (emacs "27.1") (emacsql "3.0.0") (emacsql-sqlite "3.0.0") (f "0.20.0") (ht "2.4") (transient "0.3.7") (request "0.3.3"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Version: 0.1.0
 
